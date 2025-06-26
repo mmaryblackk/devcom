a4282ee82d583e75009a5303960459edbc5a3f7f
Maryna Kravchuk <marinalinkova02@gmail.com>
2025-06-26 16:23:18 +0300

    deploy

--- src/components/Router/Router.tsx
+++ src/components/Router/Router.tsx
@@ -1,15 +1,15 @@
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { HashRouter, Navigate, Route, Routes } from "react-router-dom";
 import { CardPage } from "@/pages/CardPage";
 import { NotFoundPage } from "@/pages/NotFoundPage";
 
 export const Router = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Navigate to="/my-cards" replace />} />
         <Route path="/my-cards" element={<CardPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
